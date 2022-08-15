6a09db24b5d1a6f768c354e2a3f0705fdf292951
Nada <nadaoteif@gmail.com>
2022-08-15 15:18:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
   <link rel="stylesheet" href="style.css">
   <title>Sensor value</title>
 </head>
-
 <body>
-  
   <form class="content" action="sensor.php" method="get">
     <h2>Enter Sensor Value: </h2> 
       <input type="number" name="value" min="0" autofocus>
       <button class="submit" type="submit"> Submit </button>    
   </form>
-
 </body>
 </html>
